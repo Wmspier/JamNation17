c76ffa7dfb322116538c30dc94dab10009b0f88a
Yvanovitch <yvan.richer@gmail.com>
2017-02-12 13:02:29 -0500

    Add Arduino Read Value

---
+++ Project/Assets/Scripts/Arduino/ReadValue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+
+public class ReadValue : MonoBehaviour {
+	SerialPort sp = new SerialPort("COM4", 9600);
+	
+	void Start () {
+		sp.Open ();
+		sp.ReadTimeout = 10;
+	}
+	
+	void Update () 
+	{
+		try{
+			print (sp.ReadLine());
+		}
+		catch(System.Exception){
+		}
+		
+		
+	}
+}
